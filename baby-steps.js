c99a144165a32360f5805482857c7b1b3f92054e
thejbordo <joan.bordonaba@gmail.com>
2024-01-10 09:12:06 +0100

    stop ex 3

---
+++ baby-steps.js
@@ -0,0 +1,15 @@
+function sum(...values) {
+  const totalSum = values.reduce(
+    (valueACtual, nextValue) => valueACtual + nextValue
+  );
+  return totalSum;
+}
+
+const inputParse = (values) => {
+  const number = values.map((item) => parseInt(item));
+
+  return sum(...number);
+};
+
+const valuesInCLI = inputParse(process.argv.slice(2));
+console.log(valuesInCLI);
